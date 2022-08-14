c9aaed08234457375835198fac99b3ec4c88a9ee
Francois Malassenet <malassenet@gmail.com>
2022-08-14 21:37:54 +0200

    update options

--- nvim-config-chris/.config/nvim/lua/user/options.lua
+++ nvim-config-chris/.config/nvim/lua/user/options.lua
@@ -11,7 +11,8 @@ vim.opt.ignorecase = true                       -- ignore case in search pattern
 vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim =
 vim.opt.pumheight = 10                          -- pop up menu height =
 vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore =
-vim.opt.showtabline = 2                         -- always show tabs =
+vim.opt.showtabline = 0                         -- always show tabs =
+-- vim.opt.showtabline = 2                         -- always show tabs =
 vim.opt.smartcase = true                        -- smart case =
 vim.opt.smartindent = true                      -- make indenting smarter again =
 vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window =
@@ -19,14 +20,15 @@ vim.opt.splitright = true                       -- force all vertical splits to 
 vim.opt.swapfile = false                        -- creates a swapfile =
 vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this) =
 vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds) =
--- vim.opt.timeoutlen = 100         -- when using whichkey              -- time to wait for a mapped sequence to complete (in milliseconds) =
+-- vim.opt.timeoutlen = 100                        -- when using whichkey              -- time to wait for a mapped sequence to complete (in milliseconds) =
 vim.opt.undofile = true                         -- enable persistent undo =
-vim.opt.updatetime = 300                        -- faster completion (4000ms default) =
+vim.opt.updatetime = 100                        -- faster completion (4000ms default) =
+-- vim.opt.updatetime = 300                        -- faster completion (4000ms default) =
 vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited =
 vim.opt.expandtab = true                        -- convert tabs to spaces =
 vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation =
 vim.opt.tabstop = 2                             -- insert 2 spaces for a tab =
-vim.opt.cursorline = true                       -- highlight the current line =
+vim.opt.cursorline = false                       -- highlight the current line =
 vim.opt.number = true                           -- set numbered lines =
 vim.opt.relativenumber = true                   -- set relative numbered lines =
 vim.opt.numberwidth = 5                         -- set number column width to 2 {default 4} =
@@ -38,7 +40,12 @@ vim.opt.guifont = "firacode:h17"                -- the font used in graphical ne
 vim.opt.shortmess:append "c"
 vim.opt.whichwrap:append("<,>,[,],h,l")
 vim.opt.iskeyword:append "-"                    -- allows to consider - within the same word
-vim.opt.colorcolumn = "80"                     -- highlight column 
+vim.opt.colorcolumn = "80"                      -- highlight column 
+vim.opt.cursorline = true                       -- highlight the current line
 
 -- let g:do_filetype_lua = 1                       -- Lookup filetype.lua
 -- let g:did_load_filetypes = 0                    -- dispable filetypes.vim
+vim.opt.fillchars = 'eob: '
+vim.opt.fillchars:append{
+  stl = ' ',
+}
