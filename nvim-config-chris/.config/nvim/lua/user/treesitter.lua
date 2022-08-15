f0fefa6564b1c6f6f7dc0fc6bd5eb12a7f2721ba
Francois Malassenet <malassenet@gmail.com>
2022-08-15 07:01:22 +0200

    treesitter

--- nvim-config-chris/.config/nvim/lua/user/treesitter.lua
+++ nvim-config-chris/.config/nvim/lua/user/treesitter.lua
@@ -3,8 +3,8 @@ if not status_ok then
 	return
 end
 
-local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
-ft_to_parser.motoko = "typescript"
+-- local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+-- ft_to_parser.motoko = "typescript"
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
@@ -13,10 +13,20 @@ configs.setup({
 	highlight = {
     -- use_languagetree = true,
 		enable = true, -- false will disable the whole extension
+    disable_virtual_text = false,
 		-- disable = { "css", "html" }, -- list of language that will be disabled
 		disable = { "css", "markdown" }, -- list of language that will be disabled
 		-- additional_vim_regex_highlighting = true,
+    -- include_match_words = false,
 	},
+  highlight = {
+    -- use_languagetree = true,
+    enable = true, -- false will disable the whole extension
+    -- disable = { "css", "html" }, -- list of language that will be disabled
+    -- disable = { "css", "markdown" }, -- list of language that will be disabled
+    disable = { "markdown" }, -- list of language that will be disabled
+    -- additional_vim_regex_highlighting = true,
+  },
 	autopairs = {
 		enable = true,
 	},
@@ -27,7 +37,7 @@ configs.setup({
 	},
 	autotag = {
 		enable = true,
-		disable = { "xml" },
+		disable = { "xml", "markdown" },
 	},
 	rainbow = {
 		enable = true,
@@ -44,4 +54,65 @@ configs.setup({
 	playground = {
 		enable = true,
 	},
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["at"] = "@class.outer",
+        ["it"] = "@class.inner",
+        ["ac"] = "@call.outer",
+        ["ic"] = "@call.inner",
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["ai"] = "@conditional.outer",
+        ["ii"] = "@conditional.inner",
+        ["a/"] = "@comment.outer",
+        ["i/"] = "@comment.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["as"] = "@statement.outer",
+        ["is"] = "@scopename.inner",
+        ["aA"] = "@attribute.outer",
+        ["iA"] = "@attribute.inner",
+        ["aF"] = "@frame.outer",
+        ["iF"] = "@frame.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>."] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>,"] = "@parameter.inner",
+      },
+    },
+  },
 })
