c58326acdbcd06ff587b6904fd81f273b7312a2f
Francois Malassenet <malassenet@gmail.com>
2023-03-26 18:03:09 -0400

    nvim: updated  keys, automatic launching of dapui

--- nvim_new/.config/nvim/after/plugin/dap-ui.lua
+++ nvim_new/.config/nvim/after/plugin/dap-ui.lua
@@ -3,7 +3,6 @@ if not status_ok then
     print("dapui is not installed")
     return
 end
-print('settingup dapui')
 
 dapui.setup(
     {
@@ -13,75 +12,75 @@ dapui.setup(
             current_frame = '▸',
             circular = "↻",
         },
-        --     mappings = {
-        --         open = 'o',
-        --         remove = 'd',
-        --         edit = 'e',
-        --         repl = 'r',
-        --         toggle = 't',
-        --         expand = '<CR>',
-        --     },
-        --     controls = {
-        --         enabled = true,
-        --         element = 'repl',
-        --         icons = {
-        --             pause = "",
-        --             play = "",
-        --             step_into = "",
-        --             step_over = "",
-        --             step_out = "",
-        --             step_back = "",
-        --             run_last = "↻",
-        --             terminate = "□",
-        --         },
-        --     },
+        mappings = {
+            open = 'o',
+            remove = 'd',
+            edit = 'e',
+            repl = 'r',
+            toggle = 't',
+            expand = '<CR>',
+        },
+        controls = {
+            enabled = true,
+            element = 'repl',
+            icons = {
+                pause = "",
+                play = "",
+                step_into = "",
+                step_over = "",
+                step_out = "",
+                step_back = "",
+                run_last = "↻",
+                terminate = "□",
+            },
+        },
         --     expand_lines = vim.fn.has('nvim-0.9'),
-        --     layouts = {
-        --         {
-        --             elements = {
-        --                 { id = 'scopes', size = 0.25 },
-        --                 'breakpoints',
-        --                 'stacks',
-        --                 'watches',
-        --             },
-        --             size = 40,
-        --             position = 'left',
-        --         },
-        --         {
-        --             elements = {
-        --                 'repl',
-        --                 'console'
-        --             },
-        --             size = 0.25,
-        --             position = 'bottom',
-        --         },
-        --     },
-        --     floating = {
-        --         max_height = nil,
-        --         max_width = nil,
-        --         border = 'single',
-        --         mappings = {
-        --             close = { 'q', '<Esc>' },
-        --         },
-        --     },
-        --     windows = { indent = 1 },
-        --     render = {
-        --         max_type_length = nil,
-        --         max_value_lines = nil,
-        --     },
+        layouts = {
+            {
+                elements = {
+                    { id = 'scopes', size = 0.25 },
+                    'breakpoints',
+                    'stacks',
+                    'watches',
+                },
+                size = 40,
+                position = 'left',
+            },
+            {
+                elements = {
+                    'repl',
+                    'console'
+                },
+                size = 0.25,
+                position = 'bottom',
+            },
+        },
+        floating = {
+            max_height = nil,
+            max_width = nil,
+            border = 'single',
+            mappings = {
+                close = { 'q', '<Esc>' },
+            },
+        },
+        windows = { indent = 1 },
+        render = {
+            max_type_length = nil,
+            max_value_lines = nil,
+        },
     }
 )
 
--- local dap = require('dap')
--- dap.listeners.after_event_initialized['dapui_config'] = function()
---     dapui.open()
--- end
--- dap.listeners.after_event_terminated['dapui_config'] = function()
---     dapui.close()
--- end
--- dap.listeners.after_event_exited['dapui_config'] = function()
---     dapui.close()
--- end
+local dap = require('dap')
+dap.listeners.after.event_initialized['dapui_config'] = function()
+    dapui.open()
+end
+dap.listeners.after.event_terminated['dapui_config'] = function()
+    dapui.close()
+end
+dap.listeners.after.event_exited['dapui_config'] = function()
+    dapui.close()
+end
 
 
 -- mappings
--- nvim_new/.config/nvim/after/plugin/dap.lua
+++ nvim_new/.config/nvim/after/plugin/dap.lua
@@ -10,14 +10,14 @@ dap.defaults.fallback.force_external_terminal = true
 -- au FileType dap-repl lua require('dap.ext.autocompl').attach()
 
 -- debug continue/start
-vim.keymap.set('n', '<leader>dc', function() dap.continue() end)                                               -- continue
-vim.keymap.set('n', '<leader>dl', function() dap.run_last() end)                                               -- run lat
+vim.keymap.set('n', '<F5>', function() dap.continue() end)                                                     -- continue
+vim.keymap.set('n', '<F6>', function() dap.run_last() end)                                                     -- run lat
+vim.keymap.set('n', '<F1>', function() dap.step_into() end)                                                    --step into
+vim.keymap.set('n', '<F2>', function() dap.step_over() end)                                                    --step over
+vim.keymap.set('n', '<F3>', function() dap.step_out() end)                                                     --step out
 vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end)                                      -- toggle breakpoint
 vim.keymap.set('n', '<leader>dd', function() dap.toggle_breakpoint(vim.fn.input("Breakpoint condition ")) end) -- toggle breakpoint
 vim.keymap.set('n', '<leader>dr', function() dap.repl.open() end)                                              -- open repl
-vim.keymap.set('n', '<leader>dn', function() dap.step_over() end)                                              --step over
-vim.keymap.set('n', '<leader>di', function() dap.step_into() end)                                              --step into
-vim.keymap.set('n', '<leader>do', function() dap.step_out() end)                                               --step out
 -- may be moved with python
 vim.keymap.set('n', '<leader>dm', function() dap.test_method() end)                                            -- test method
 vim.keymap.set('n', '<leader>dk', function() dap.test_class() end)                                             -- test class
