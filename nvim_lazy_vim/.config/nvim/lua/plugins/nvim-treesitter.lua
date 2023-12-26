5dc79c40c98499549362f851febfea6b2e0c4f8c
Francois Malassenet <malassenet@gmail.com>
2023-12-26 10:07:17 +0100

    lazy.nvim nvim-treesitter

---
+++ nvim_lazy_vim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,67 @@
+local config = function()
+    require("nvim-treesitter.configs").setup({
+        build = ":TSUpdate",
+        indent = {
+            enable = true,
+        },
+        autotag = { 
+            enable = true,
+        },
+        ensure_installed = {
+            "markdown",
+            "json",
+            "markdown",
+            "json",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "csv",
+            "bash",
+            "lua",
+            "luadoc",
+            "dockerfile",
+            "gitignore",
+            "python",
+            "cpp",
+            -- "glsl",
+            -- "hlsl",
+            "cmake",
+            "vim",
+            "vimdoc",
+            "latex",
+            "bibtex",
+            "c_sharp",
+            "gitcommit",
+            "git_config",
+            "git_rebase",
+            "gitattributes",
+            "go",
+            "rust",
+            "toml",
+            -- "wsgl",
+            -- "wsgl_bevy",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlight = true,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<C-s>",
+                node_incremental = "<C-s>",
+                scope_incremental = false, 
+                scope_decremental = "<bs>",
+            },
+        },
+    })
+end
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    config = config,
+}
+
