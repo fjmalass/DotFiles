383a7b2ff6e54fb87c9508734840627425e82117
Francois Malassenet <malassenet@gmail.com>
2023-12-26 12:28:28 +0100

    lazy.nvim lualine

--- nvim_lazy_vim/.config/nvim/lua/plugins/noice.lua
+++ nvim_lazy_vim/.config/nvim/lua/plugins/noice.lua
@@ -9,7 +9,9 @@ return {
             "rcarriga/nvim-notify",
             config = function() -- to avoid transparency error
                 require("notify").setup({
-                    background_colour = "#000000"
+                    background_colour = "#000000",
+                    timeout=1000,
+                    render="wrapped-compact",
                 })
             end,
         },
