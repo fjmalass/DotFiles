0bbc56455fdff2f0e8ee12730537360159c77c00
Francois Malassenet <malassenet@gmail.com>
2023-12-26 09:01:46 +0100

    lazy.nvim: devicons, telescope, wip nvim-tree to show git icons

--- nvim_lazy_vim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim_lazy_vim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,61 @@
+local g = vim.g
+
+g.nvim_tree_add_trailing = 1 --append a trailing slash after a directory
+g.nvim_tree_git_hl = true
+g.nvim_tree_icons = {
+   default = "",
+   symlink = "",
+   git = {
+      deleted = "",
+      ignored = "◌",
+      renamed = "➜",
+      staged = "✓",
+      unmerged = "",
+      unstaged = "✗",
+      untracked = "★",
+   },
+   folder = {
+      default = "",
+      empty = "",
+      empty_open = "",
+      open = "",
+      symlink = "",
+      symlink_open = "",
+   },
+}
+
+-- specific key mapping
+--
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+    local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up Parent"))
+    vim.keymap.set("n", "?", api.tree.toggle_help, opts("help"))
+
+end
 return {
     "nvim-tree/nvim-tree.lua",
     lazy = false,
+    on_attach = on_attach,
     config = {
         filters = {
             dotfiles = false,
-        }
+        },
+        hijack_netrw = true,
+        disable_netrw = true,
+        open_on_tab = false,
+        hijack_cursor = true,
+        update_cwd = true,
+        update_focused_file = {
+            enable = true,
+            update_cwd = false,
+        },
+        view = default_ui,
+        git = {
+            ignore = false,
+        },
     }
 }
---
+++ nvim_lazy_vim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,47 @@
+-- Make sure to install ripgrep
+local keymap = vim.keymap
+
+local config = function()
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-j>"] = "move_selection_next",
+					["<C-k>"] = "move_selection_previous",
+				},
+			},
+		},
+		pickers = {
+			find_files = {
+				-- theme = "dropdown",
+				previewer = true,
+				hidden = true,
+			},
+			live_grep = {
+				-- theme = "dropdown",
+				previewer = true,
+			},
+			buffers = {
+				-- theme = "dropdown",
+				previewer = true,
+			},
+		},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }, 
+	})
+end
+
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = config,
+	keys = {
+		keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>"),
+		keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>"),
+		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>"),
+		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>"),
+		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>"),
+	},
+}
