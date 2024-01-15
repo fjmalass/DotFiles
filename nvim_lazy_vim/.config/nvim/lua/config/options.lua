4b3e4f983e30ab6c8696c8f1e43e7c8ef8b0f432
Francois Malassenet <malassenet@gmail.com>
2024-01-15 09:53:09 +0100

    Added - and _ to word motions

--- nvim_lazy_vim/.config/nvim/lua/config/options.lua
+++ nvim_lazy_vim/.config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@ local opt = vim.opt
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
-opt.expandtab = true
+opt.expandtab = true -- converts tabs to spaces
 opt.smartindent = true
 opt.wrap = false
 
@@ -27,6 +27,12 @@ opt.completeopt = "menuone,noinsert,noselect"
 opt.updatetime = 50
 opt.isfname:append("@-@")
 
+-- Pop Up Menu
+-- opt.pumheight = 10 -- pop up menu height
+-- opt.pumblend = 10
+opt.fillchars = opt.fillchars + "eob: " -- change the end of buffer from ~ to space
+opt.fillchars:append({ stl = " " }) -- change status line to have space
+
 -- Behaviour
 opt.hidden = true
 opt.errorbells = false
@@ -47,8 +53,13 @@ opt.guicursor =
 	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 opt.encoding = "UTF-8"
 opt.showmode = false
+opt.showtabline = 1 -- always show tabs
+opt.writebackup = false -- if a file is being edited by another program
 
 -- folds
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 99
+
+-- word selection
+opt.iskeyword:append("-,_") -- adds - and _ as part of a word when moving/selecting words
