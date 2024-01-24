fd5fb6baca40a43e089bd582b1e174939124c388
Francois Malassenet <malassenet@gmail.com>
2024-01-24 15:53:35 +0100

    lazy.vim: changed trouble to <leader>xx etc

--- nvim_lazy_vim/.config/nvim/lua/plugins/trouble.lua
+++ nvim_lazy_vim/.config/nvim/lua/plugins/trouble.lua
@@ -1,12 +1,41 @@
 local keys = function()
 	local trouble = require("trouble")
-	vim.keymap.set("n", "<leader>tt", trouble.toggle, { desc = "Trouble: Toggle" })
-	vim.keymap.set("n", "<leader>tn", function()
+	vim.keymap.set("n", "<leader>xx", trouble.toggle, { desc = "Trouble: Toggle" })
+    -- next
+	vim.keymap.set("n", "<leader>xn", function()
 		trouble.next({ skip_groups = true, jump = true })
 	end, { desc = "Trouble: Next" })
-	vim.keymap.set("n", "<leader>tp", function()
+    -- previous
+	vim.keymap.set("n", "<leader>xp", function()
 		trouble.previous({ skip_groups = true, jump = true })
 	end, { desc = "Trouble: Prev" })
+    -- first
+	vim.keymap.set("n", "<leader>xf", function()
+		trouble.first({ skip_groups = true, jump = true })
+	end, { desc = "Trouble: First" })
+    -- last
+	vim.keymap.set("n", "<leader>xl", function()
+		trouble.last({ skip_groups = true, jump = true })
+	end, { desc = "Trouble: Last" })
+	vim.keymap.set("n", "<leader>xd", function()
+		trouble.toggle("document_diagnostics")
+	end, { desc = "Trouble: Toggle Document" })
+    -- workspace
+	vim.keymap.set("n", "<leader>xw", function()
+		trouble.toggle("workspace_diagnostics")
+	end, { desc = "Trouble: Toggle Workspace" })
+    -- quick fix
+	vim.keymap.set("n", "<leader>xq", function()
+		trouble.toggle("quickfix")
+	end, { desc = "Trouble: Toggle QuickFix" })
+    -- loc list
+	vim.keymap.set("n", "<leader>xl", function()
+		trouble.toggle("loclist")
+	end, { desc = "Trouble: Toggle LocList" })
+    -- lsp references
+	vim.keymap.set("n", "gR", function()
+		trouble.toggle("lsp_references")
+	end, { desc = "Trouble: Lsp References" })
 end
 
 return {
